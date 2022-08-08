41408c04badc2a05ceccdf20ae6e271dcb8476fe
Jesus Clemente <jcdeveloper92@gmail.com>
2022-08-07 21:33:11 -0400

    added isLoading state

--- src/assets/components/CardWeather.jsx
+++ src/assets/components/CardWeather.jsx
@@ -1,10 +1,12 @@
 import { React, useState, useEffect } from "react";
+import { LoadingInfo } from "./LoadingInfo";
 import axios from "axios";
 
 const CardWeather = ({ lat, lon }) => {
   const [weather, setWeather] = useState({});
   const [temp, setTemp] = useState({});
   const [isCelsius, setFahrenheit] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [background, setBackground] = useState();
 
   useEffect(() => {
@@ -17,22 +19,19 @@ const CardWeather = ({ lat, lon }) => {
           setWeather(response.data);
           const temp = {
             kelvin: response.data.main.temp,
-            fahrenheit: Math.round(response.data.main.temp * 9 / 5 - 459.67),
+            fahrenheit: Math.round((response.data.main.temp * 9) / 5 - 459.67),
             celsius: Math.round(response.data.main.temp - 273.15),
-          }
+          };
           setTemp(temp);
+          setIsLoading(false);
         })
         .catch((error) => console.log(error));
     }
   }, [lat, lon]);
 
-
-
   const handleChange = () => {
     isCelsius ? setFahrenheit(false) : setFahrenheit(true);
   };
-
-
 
   // let backgroundClass = {
   //   backgroundImage: `url(${background})`,
@@ -43,46 +42,51 @@ const CardWeather = ({ lat, lon }) => {
   //   width: "100vw",
   // }
 
-  return (
-    <div>
-      <div className="card">
-        <h1>Weather App</h1>
-        <div className="card-body">
-          <h2 className="card-title">{weather.name}</h2>
-          <p className="card-text" id="temperature">
-            <span>
-              <i className="fas fa-thermometer-three-quarters"></i>
-            </span>
-            <strong>Temperature:</strong>{" "}
-            {isCelsius ? temp.celsius : temp.fahrenheit} {isCelsius ? "C" : "F"}
-            &deg;
-          </p>
-          <p className="card-text">
-            <span>
-              <i className="fas fa-cloud"></i>
-            </span>
-            <strong>Clouds:</strong> {weather.weather?.[0].description}{" "}
-            {weather.clouds?.all}%
-          </p>
-          <p className="card-text">
-            <span>
-              <i className="fas fa-wind"></i>
-            </span>
-            <strong>Wind speed:</strong> {weather.wind?.speed} m/s
-          </p>
-          <p className="card-text">
-            <span>
-              <i className="fa-solid fa-temperature-low"></i>
-            </span>
-            <strong>Pressure:</strong> {weather.main?.pressure} mb
-          </p>
+  if (isLoading) {
+    return <LoadingInfo />;
+  } else {
+    return (
+      <div>
+        <div className="card">
+          <h1>Weather App</h1>
+          <div className="card-body">
+            <h2 className="card-title">{weather.name}</h2>
+            <p className="card-text" id="temperature">
+              <span>
+                <i className="fas fa-thermometer-three-quarters"></i>
+              </span>
+              <strong>Temperature:</strong>{" "}
+              {isCelsius ? temp.celsius : temp.fahrenheit}{" "}
+              {isCelsius ? "C" : "F"}
+              &deg;
+            </p>
+            <p className="card-text">
+              <span>
+                <i className="fas fa-cloud"></i>
+              </span>
+              <strong>Clouds:</strong> {weather.weather?.[0].description}{" "}
+              {weather.clouds?.all}%
+            </p>
+            <p className="card-text">
+              <span>
+                <i className="fas fa-wind"></i>
+              </span>
+              <strong>Wind speed:</strong> {weather.wind?.speed} m/s
+            </p>
+            <p className="card-text">
+              <span>
+                <i className="fa-solid fa-temperature-low"></i>
+              </span>
+              <strong>Pressure:</strong> {weather.main?.pressure} mb
+            </p>
+          </div>
+          <button className="btn btn-primary" onClick={handleChange}>
+            Switch {isCelsius ? "F" : "C"} &deg;
+          </button>
         </div>
-        <button className="btn btn-primary" onClick={handleChange}>
-          Switch {isCelsius ? "F" : "C"} &deg;
-        </button>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default CardWeather;
---
+++ src/assets/components/LoadingInfo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const LoadingInfo = () => {
+  return (
+    <div className="loading-info">
+        loading...
+        <i className="fas fa-spinner fa-spin"></i>
+    </div>
+  )
+}
